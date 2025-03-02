6dfdda68d9a27365cb1a8667b1eed25d650f1a8c
Agi <abdghifary@gmail.com>
2025-03-03 01:55:08 +0700

    install tailwind and create github api service

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="@rsbuild/core/types" />
+declare namespace NodeJS {
+  interface ProcessEnv {
+    PUBLIC_GIT_TOKEN: string;
+  }
+}
---
+++ src/services/git-api-service.ts
@@ -0,0 +1,61 @@
+import { Octokit } from 'octokit';
+import { Endpoints, RequestParameters } from '@octokit/types';
+
+type SearchUserRequestParameters = {
+  q: string;
+  sort?: 'followers' | 'repositories' | 'joined';
+  order?: 'desc' | 'asc';
+  per_page?: number;
+  page?: number;
+} & RequestParameters;
+
+export type UserSearchResponse =
+  Endpoints['GET /search/users']['response']['data'];
+export type UserRepository =
+  Endpoints['GET /users/{username}/repos']['response']['data'][0];
+export type Repositories = UserRepository[];
+
+export const useGitApiService = () => {
+  const octokit = new Octokit({
+    auth: process.env.PUBLIC_GIT_TOKEN,
+  });
+
+  const searchUser = async (
+    data: SearchUserRequestParameters,
+  ): Promise<UserSearchResponse> => {
+    try {
+      const response = await octokit.request('GET /search/users', {
+        q: data.q,
+        sort: data.sort,
+        order: data.order,
+        per_page: data.per_page,
+        page: data.page,
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error('Error searching users:', error);
+      throw error;
+    }
+  };
+
+  const searchUserRepositories = async (
+    username: string,
+  ): Promise<Repositories> => {
+    try {
+      const response = await octokit.request('GET /users/{username}/repos', {
+        username,
+      });
+
+      return response.data;
+    } catch (error) {
+      console.error('Error searching user repositories:', error);
+      throw error;
+    }
+  };
+
+  return {
+    searchUser,
+    searchUserRepositories,
+  };
+};
